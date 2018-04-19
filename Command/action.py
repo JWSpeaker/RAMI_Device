e8ddf8c7ccccd8f0965527fd42161c200252a8b8
Ryu <rjch221@gmail.com>
2018-04-18 21:06:50 -0700

    종료안되게`

--- Command/action.py
+++ Command/action.py
@@ -4,8 +4,7 @@ def case(x):
     return {
         '오늘날씨' : GetWeatherInfo(),
         '현재시간' : GetDatetime(),
-        '종료' : 'exit'
-
+        '종료' : 'exit1'
     }.get(x, 'not defined command')
 
 
